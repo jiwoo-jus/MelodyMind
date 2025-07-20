fa48d14b8e66eb674e387c9417944b7524acf4f4
cassrose19 <rose.1399@osu.edu>
2025-07-20 14:27:40 -0400

    Add genre mapping dictionary to group common subgenres into more general genres

---
+++ app/scripts/common_genres.py
@@ -0,0 +1,40 @@
+COMMON_GENRES = {
+    "Pop": [
+        "pop", "dance pop", "bubblegum pop", "post-teen pop",
+        "brill building pop", "new wave pop", "pop rock"
+    ],
+    "Rock": [
+        "rock", "modern rock", "classic rock", "pop punk", "soft rock",
+        "hard rock", "art rock", "folk rock", "roots rock", "album rock",
+        "dance rock", "indie rock", "alternative rock", "blues-rock"
+    ],
+    "Hip-Hop / Rap": [
+        "rap", "hip hop", "pop rap", "gangster rap", "southern hip hop",
+        "trap music", "hip pop", "atl hip hop", "dfw rap"
+    ],
+    "R&B / Soul": [
+        "r&b", "soul", "funk", "urban contemporary", "quiet storm",
+        "motown", "classic soul", "neo soul"
+    ],
+    "Country": [
+        "country", "country rock", "contemporary country"
+    ],
+    "Folk / Indie": [
+        "folk", "folk-pop", "indie folk", "singer-songwriter",
+        "indie pop", "mellow gold"
+    ],
+    "Electronic / Dance": [
+        "disco", "house", "electro", "brostep", "dance", "edm",
+        "progressive house", "electronic"
+    ],
+    "Alternative / Metal": [
+        "alternative metal", "nu metal", "post-grunge", "emo",
+        "hardcore", "grunge"
+    ],
+    "Holiday": [
+        "adult standards", "vocal jazz", "lounge", 
+        "traditional country", "cowboy western",
+        "easy listening", "american folk revival"
+    ],
+    "Other": []
+}
