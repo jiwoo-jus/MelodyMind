dc0c9cfac211d0466c3c45dba8c68aa7dcb82e20
cassrose19 <rose.1399@osu.edu>
2025-07-21 14:45:05 -0400

    updated genre mappings by adding more keywords to genres

--- app/scripts/common_genres.py
+++ app/scripts/common_genres.py
@@ -1,40 +1,45 @@
 COMMON_GENRES = {
     "Pop": [
         "pop", "dance pop", "bubblegum pop", "post-teen pop",
-        "brill building pop", "new wave pop", "pop rock"
+        "brill building pop", "new wave pop", "pop rock",
+        "europop", "classic uk pop", "latin pop", "sunshine pop",
+        "bow pop"
     ],
     "Rock": [
         "rock", "modern rock", "classic rock", "pop punk", "soft rock",
         "hard rock", "art rock", "folk rock", "roots rock", "album rock",
-        "dance rock", "indie rock", "alternative rock", "blues-rock"
+        "dance rock", "indie rock", "alternative rock", "blues-rock",
+        "rock-and-roll", "classic garage rock", "classic canadian rock",
+        "australian rock", "deep soft rock", "yacht rock", 
+        "christian alternative rock"
     ],
     "Hip-Hop / Rap": [
         "rap", "hip hop", "pop rap", "gangster rap", "southern hip hop",
-        "trap music", "hip pop", "atl hip hop", "dfw rap"
+        "trap music", "hip pop", "atl hip hop", "dfw rap", "emo rap",
+        "miami hip hop"
     ],
     "R&B / Soul": [
         "r&b", "soul", "funk", "urban contemporary", "quiet storm",
-        "motown", "classic soul", "neo soul"
+        "motown", "classic soul", "neo soul", "southern soul",
+        "soul jazz", "northern soul", "jazz funk", "smooth jazz",
+        "vocal jazz"
     ],
     "Country": [
-        "country", "country rock", "contemporary country"
+        "country", "country rock", "contemporary country", "traditional country",
+        "nashville sound", "cowboy western", "country gospel"
     ],
     "Folk / Indie": [
         "folk", "folk-pop", "indie folk", "singer-songwriter",
-        "indie pop", "mellow gold"
+        "indie pop", "mellow gold", "americal folk revival"
     ],
     "Electronic / Dance": [
         "disco", "house", "electro", "brostep", "dance", "edm",
-        "progressive house", "electronic"
+        "progressive house", "electronic", "eurodance", "hip house",
+        "freestyle", "dancehall", "deep disco"
     ],
     "Alternative / Metal": [
         "alternative metal", "nu metal", "post-grunge", "emo",
         "hardcore", "grunge"
-    ],
-    "Holiday": [
-        "adult standards", "vocal jazz", "lounge", 
-        "traditional country", "cowboy western",
-        "easy listening", "american folk revival"
     ],
     "Other": []
 }
